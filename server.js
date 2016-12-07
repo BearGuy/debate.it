9e19a3e367ff357f8b7ef6505f53ee10410f54fe
LiDaEn <engliam95@gmail.com>
2016-12-06 23:11:43 -0500

    Added User routes.
    Beware of mongoose.

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(bodyParser.json());
 var port = process.env.PORT || 8080 //set out port
 
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://superAdmin:4s5u5j@localhost/debater',{auth:{authdb:"admin"}});
+mongoose.connect('mongodb://LiDaEn:mypass4db@localhost/debater',{auth:{authdb:"admin"}});
 mongoose.set('debug', true);
 
 var router = express.Router()
@@ -60,7 +60,7 @@ router.route('/bears/:bear_id')
     .get(function(req, res) {
         Bear.findById(req.params.bear_id, function(err, bear) {
             if (err)
-                res.send(err)
+                res.send(err);
             res.json(bear);
         });
     })
@@ -103,3 +103,80 @@ app.use('/api', router);
 app.listen(port);
 console.log('The Magic is happening on port ' + port)
 
+// User routes below this line.
+  router.route('/users')
+
+    //create a user**
+    .post(function(req, res) {
+
+      var user = new User(); // create a new instance of a user model.
+      user.name = req.body.name; //sets name.
+      user.email = req.body.email; //submit email.
+      user.username = req.body.username; //set username.
+      user.password = req.body.password; //set password.
+
+
+      //save user & check for errors
+      user.save(function(err) {
+        if (err)
+          res.send(err);
+
+        res.json({ message: 'User created.'});
+      });
+
+    })
+
+    .get(function(req, res) {
+      User.find(function(err, user) {
+        if(err)
+          res.send(err);
+
+        res.json(user);
+      });
+    });
+
+  router.route('/users/:user_id')
+
+    //get the username with user_id.
+    .get(function(req, res) {
+      User.findById(req.params.user_id, function(err, user) {
+        if (err)
+          res.send(err);
+        res.json(user);
+      });
+  })
+
+  .put(function(req, res) {
+
+    //use user model to find wanted user.
+    User.findById(req.params.user_id, function(err, user) {
+        if (err)
+          res.send(err);
+
+      user.name = req.body.name; // update user info.
+
+      //save the user.
+
+      user.save(function(err) {
+        if (err)
+          res.send(err);
+
+          res.json({ message: 'User updated.' });
+      });
+
+    });
+
+})
+
+    //delete-a-user.
+
+    .delete(function(req, res) {
+      User.remove({
+        _id: req.params.user_id
+      }, function(err, bear) {
+        if (err)
+          res.send(err);
+
+          res.json({message: 'User has been vaporized.'});
+      });
+    });
