5e09ec884d4744728fc2349059f190524d260bc5
BearGuy <stephen.peterkins@shopify.com>
2016-12-06 23:29:15 -0500

    Shoddily included Liams changes

--- config/initializers/server.js
+++ config/initializers/server.js
@@ -7,6 +7,7 @@ var rsaValidation = require('auth0-api-jwt-rsa-validation');
 
 var bodyParser = require('body-parser');
 var Bear       = require('../../app/models/bear')
+var User      = require ('../../app/models/user')
 
 var start = function(cb) {
 
@@ -97,11 +98,88 @@ var start = function(cb) {
             });
         });
 
+        // User routes below this line.
+    router.route('/users')
+
+    //create a user**
+        .post(function(req, res) {
+
+            var user = new User(); // create a new instance of a user model.
+            user.name = req.body.name; //sets name.
+            user.email = req.body.email; //submit email.
+            user.username = req.body.username; //set username.
+            user.password = req.body.password; //set password.
+
+
+      //save user & check for errors
+          user.save(function(err) {
+            if (err)
+              res.send(err);
+
+            res.json({ message: 'User created.'});
+          });
+
+        })
+
+        .get(function(req, res) {
+          User.find(function(err, user) {
+            if(err)
+              res.send(err);
+
+            res.json(user);
+          });
+        });
+
+      router.route('/users/:user_id')
+
+        //get the username with user_id.
+        .get(function(req, res) {
+          User.findById(req.params.user_id, function(err, user) {
+            if (err)
+              res.send(err);
+            res.json(user);
+          });
+      })
+
+      .put(function(req, res) {
+
+        //use user model to find wanted user.
+        User.findById(req.params.user_id, function(err, user) {
+            if (err)
+              res.send(err);
+
+          user.name = req.body.name; // update user info.
+
+          //save the user.
+
+          user.save(function(err) {
+            if (err)
+              res.send(err);
+
+              res.json({ message: 'User updated.' });
+          });
+
+        });
+
+    })
+
+    //delete-a-user.
+
+        .delete(function(req, res) {
+          User.remove({
+            _id: req.params.user_id
+          }, function(err, bear) {
+            if (err)
+              res.send(err);
+
+              res.json({message: 'User has been vaporized.'});
+          });
+        });
 
     app.use('/api', router);
 
     app.listen(8080);
-    console.log('The Magic is happening on port ' + '8080')
+    console.log('The Magic is happening on port 8080')
 
     if (cb){
         return cb
