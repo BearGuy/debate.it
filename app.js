67423f02c07de1531792e93db720bfcf8e86ded2
Bearguy <stephen@peterkins.ca>
2017-07-05 20:26:53 -0400

    Reverted naming scheme for app.js

--- app.js
+++ app.js
@@ -1,69 +1,34 @@
-//To be used
-var express       = require('express');
-var app           = express();
-var path          = require('path');
-var cors          = require('cors');
-//var config        = require('nconf');
+// /index.js
+'use strict';
 
-var expressJWT    = require('express-jwt');
+var server = require('./config/initializers/server'); //config/initializers/server
+var nconf = require('nconf');
+var async = require('async');
+var logger = require('winston');
 
-var jwt           = require('jsonwebtoken');
-var rsaValidation = require('auth0-api-jwt-rsa-validation');
+// Load Environment variables from .env file
+//require('dotenv').load();
 
-var bodyParser    = require('body-parser');
+// Set up configs
+nconf.use('memory');
+// First load command line arguments
+nconf.argv();
+// Load environment variables
+//nconf.env();
+// Load config file for the environment
+//require('./config/environments/' + nconf.get('NODE_ENV'));
 
-var apiRoutes     = require('./app/routes/index')
+logger.info('[APP] Starting server initialization');
 
-const secret      = require('./app/auth/secret');
-
-var start = function(cb) {
-
-  app.use(cors());
-
-  app.use(bodyParser.urlencoded({extended : true}));
-  app.use(bodyParser.json());
-
-  app.use(expressJWT({ secret: secret }).unless({
-    path: ['/',
-      { url: '/api/events', methods: ['GET'] },
-      { url: '/api/auth', methods: ['POST'] },
-      { url: '/api/users', methods: ['POST'] }
-    ]
-  }));
-
-  app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname+'/html/home.html'));
-  });
-
-  app.use('/api', apiRoutes);
-
-  // catch 404 and forward to error handler
-  app.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
-  });
-
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.json({
-      message: err.message,
-      error: (app.get('env') === 'development' ? err : {})
-    });
-    next(err);
-  });
-
-  // development error handler
-  // will print stacktrace
-
-  var port = 8080 //config.get('NODE_PORT')
-  app.listen(process.env.PORT || port);
-  console.log('The Magic is happening on port ' + port + ' or 5000 on heroku')
-
-  if (cb){
-    return cb
+// Initialize Modules
+async.series([
+  function startServer(callback) {
+    server(callback);
+  }], function(err) {
+    if (err) {
+      logger.error('[APP] initialization failed', err);
+    } else {
+      logger.info('[APP] initialized SUCCESSFULLY');
+    }
   }
-
-};
-
-module.exports = start;
+);
